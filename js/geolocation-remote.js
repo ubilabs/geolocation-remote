a01a571b4f65d728283b48f3e0a95f85bbeddebd
Martin Kleppe <kleppe@ubilabs.net>
2012-10-11 11:45:51 +0200

    Add inline comments.

--- js/geolocation-remote.js
+++ js/geolocation-remote.js
@@ -1,21 +1,26 @@
 function geolocationRemote(socketUrl){
 
   var socket = io.connect(socketUrl),
-    position,
     watchers = {},
+    position,
     id = 0;
 
-  socket.on('update', onUpdate);
-
-  function onUpdate (coords) {
+  // Listen for updates via the socket connection.
+  socket.on('update', function (coords) {
     position = {
       coords: coords,
       timestamp: new Date()
     };
 
     updateWatchers();
+  });
+
+  // Send an update to all connected clients.
+  function updatePosition (coords){
+    socket.emit("update", coords);
   }
 
+  // Execute all success handlers.
   function updateWatchers (){
     var watcher;
 
@@ -25,10 +30,8 @@ function geolocationRemote(socketUrl){
     }
   }
 
-  function updatePosition (coords){
-    socket.emit("update", coords);
-  }
-
+  // Return the current position.
+  // If no poisition is available it waits until the first update.
   function getCurrentPosition (success, error, options){
     if (position){
       success(position);
@@ -40,15 +43,18 @@ function geolocationRemote(socketUrl){
     }
   }
 
+  // Watches all position updates.
   function watchPosition (success, error, options){
     watchers[++id] = { id: id, success: success, error: error };
     return id;
   }
 
+  // Clears a watcher by it's id.
   function clearWatch (id){
     delete watchers[id];
   }
 
+  // Return the public interface.
   return {
     updatePosition: updatePosition,
     getCurrentPosition: getCurrentPosition,
