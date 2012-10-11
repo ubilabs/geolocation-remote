7fbc0c8d1f51ecd8f1cfdebfc25f2bc1f08722cc
Martin Kleppe <kleppe@ubilabs.net>
2012-10-11 11:46:41 +0200

    Emit the last known position to clients.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var io = require('socket.io').listen(8888);
+var port = 8888,
+  io = require('socket.io').listen(port),
+  current;
 
 io.sockets.on('connection', function (socket) {
   socket.join("room");
 
   socket.on('update', function (data) {
     io.sockets.in('room').emit('update', data);
+    current = data;
   });
+
+  if (current){
+    socket.emit('update', current);
+  }
 });
