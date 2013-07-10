3ba0194491aa1639063a0ef961c7f82807b122c1
Frank Mecklenburg Ubilabs <mecklenburg@ubilabs.net>
2013-07-10 16:55:03 +0200

    Complete rework of remote-app.js

--- app/scripts/remote-app.js
+++ app/scripts/remote-app.js
@@ -1,148 +1,43 @@
-/**
- * Load remote modules for testing the app with a Google Maps
- */
+var App = Model({
 
-var remote = {
   error: -1,
-  online: true
-};
+  onLine: true,
 
-var webapp = {
-  src: window.location.origin
-};
+  defaultPosition: {
+    "latitude": 53.56,
+    "longitude": 9.96
+  },
 
-$(function() {
+  init: function() {
+  },
 
-  remote.defaults = {
-    distance: 1000,
-    minMovement: 8,
-    angle: 80,
-    position: {
-      "lng": 9.96,
-      "lat": 53.56
-    }
-  };
+  getPosition: function () {
+    // get current pos from real navigator or use backup
+    navigator.geolocation.getCurrentPosition(this.getPosSuccess, this.getPosError);
+  },
 
-  remote.map = new MapModel();
-  remote.route = new RouteModel();
-  remote.controls = new ControlsModel();
+  getPosSuccess: function (position) {
+    this.position = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    };
 
-  webapp.marker = {};
+    remoteLog('appPosition:init');
+    this.trigger('appPosition:init');
+  },
 
-  remote.socket = io.connect(
-    document.location.protocol + "//" +
-    document.location.hostname + ":" +
-    8888
-  );
+  getPosError: function () {
+    this.position = this.defaultPosition
 
-  remote.controls.enable();
-  remote.map.addCenter(remote.defaults.position, true);
-  // remote.controls.updateRoute(pos1, pos2, geocode true||false);
+    remoteLog('appPosition:init');
+    this.trigger('appPosition:init');
+  },
 
-  remote.socket.on('update:remote', function (data) {
-    console.log('update:remote @remote');
-    if (data.init) {
-      updateWebapp();
-    } else if (data.pois) {
-      remote.map.addPois(data.pois);
-    } else {
-      webapp.marker = data;
-    }
-  })
-
-  updateIframe(window.location.search);
-  initTestEvents();
-
-  var $webappControl = $('.webapp'),
-    $webappUrlInput = $webappControl.find('.url');
-
-  $('.set-webapp').on('click', function (e) {
-    updateIframe($webappUrlInput.val());
-  })
-
-  remote.controls.updateSpeed();
-  remote.controls.updateAccuracy();
-});
-
-function updateWebapp (data) {
-
-  var data = data || {},
-    latLng = remote.map.latLng;
-
-  data.error = remote.error;
-  data.online = remote.online;
-
-  data.position = {
-    coords: {
-      latitude: latLng && latLng.lat(),
-      longitude: latLng && latLng.lng(),
-      accuracy: remote.route.driving.accuracy
-    },
-    timestamp: new Date().getTime()
-  }
-  remote.socket.emit("update:navigator", data);
-}
-
-function updateIframe (query) {
-
-  // prevent me from getting framed all over again and again and again ...
-  if (window != window.top) {
-    /* I'm in a frame! */
-    return;
+  updatePosition: function (latLng) {
+    this.position = {
+      latitude: latLng.lat(),
+      longitude: latLng.lng()
+    };
   }
 
-  var src = webapp.src;
-  var query = /\?/.test(query) ? query : '?' + query
-
-  src = query ? src + query + '&remote=true' : src + '?remote=true';
-  src = (/http:\/\//.test(src)) ? src : 'http://' + src;
-
-  $('.webapp .url').val(query);
-
-  $('iframe#webapp').load(function() {
-    injectJavascript();
-  });
-
-  $('iframe#webapp').attr('src', src);
-
-
-}
-
-function injectJavascript () {
-
-  var script, iframe;
-
-  var iframe = document.getElementById("webapp");
-
-  var jsLibs = [
-    '/remote/scripts/socket.io.min.js',
-    '/remote/scripts/remote-geolocation.js',
-    '/remote/scripts/remote-navigator.js'
-  ]
-
-  $.each(jsLibs, function (i, src) {
-
-    script = iframe.contentWindow.document.createElement("script");
-    script.type = "text/javascript";
-    script.src = src;
-    iframe.contentWindow.document.body.appendChild(script);
-
-  })
-}
-
-function initTestEvents() {
-  remote.controls.on('place:changed', remote.route.updateRoutePlaces);
-  remote.controls.on('speed:changed', remote.route.updateSpeed);
-  remote.controls.on('accuracy:changed', remote.route.updateAccuracy);
-  remote.controls.on('drive:reset', remote.route.resetDriving);
-  remote.controls.on('drive:start', remote.route.startDriving);
-  remote.controls.on('drive:stop', remote.route.stopDriving);
-
-  remote.route.on('map:center', remote.map.updateCenter);
-  remote.route.on('update', remote.map.updateRoute);
-  remote.route.on('marker:update', remote.map.updateCenter);
-  remote.route.on('drive:started', remote.controls.onDriveStarted);
-  remote.route.on('drive:stopped', remote.controls.onDriveStopped);
-
-  remote.map.on('route:changed', remote.route.onRouteChange);
-}
+})
