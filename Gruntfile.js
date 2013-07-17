b41954af3707dafec02c166d8b077bb5e85f13f5
Frank Mecklenburg Ubilabs <mecklenburg@ubilabs.net>
2013-07-17 16:35:21 +0200

    Make grunt server accessable from the outside

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
             options: {
                 port: 8000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
