2c12433b6e97acfba40365657fb14e3abe1bc69b
Frank Mecklenburg Ubilabs <mecklenburg@ubilabs.net>
2013-07-18 10:59:06 +0200

    Add changes as suggested in Issue #2

--- app/scripts/remote-route.js
+++ app/scripts/remote-route.js
@@ -57,15 +57,15 @@ var RouteModel = Model({
   updateRoute: function(routeDetails) {
     this.route.path = [];
 
-    $.each(routeDetails.legs, _.bind(function(i, leg) {
-      $.each(leg.steps, _.bind(function(k, step) {
-        $.each(step.path, _.bind(function(l, point) {
+    _.each(routeDetails.legs, function(leg, i) {
+      _.each(leg.steps, function(step, k) {
+        _.each(step.path, function(point, l) {
           if (l !== 0 || (i === 0 && k === 0 && l === 0)) {
             this.route.path.push(point);
           }
-        }, this));
-      }, this));
-    }, this));
+        }, this);
+      }, this);
+    }, this);
 
     this.calculateStepDistances();
   },
